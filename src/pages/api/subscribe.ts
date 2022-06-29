8bd6994c4129872db4fef8542aec75c09dd62297
maia.everton22@gmail.com <maia.everton22@gmail.com>
2022-06-29 15:22:21 -0300

    single check payment and user registration

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,22 +1,59 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
+import { query as q } from "faunadb";
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
 
+type User = {
+    ref: {
+        id: string,
+    }
+        data: {
+        stripe_customer_id: string,
 
-export default async (req: NextApiRequest, res:NextApiResponse) => {
+        }
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     if (req.method === 'POST') {
 
         const session = await getSession({ req })
 
-        const stripeCustomer = stripe.customers.create({
-            email: session.user.email,
-        })
+        const user = await fauna.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(session.user.email)
+                )
+            )
+        )
+
+        let customerId = user.data.stripe_customer_id
+
+        if(!customerId){
+
+            const stripeCustomer = stripe.customers.create({
+                email: session.user.email,
+            })
+            await fauna.query(
+                q.Update(
+                    q.Ref(q.Collection('users'), user.ref.id),
+                    {
+                        data:
+                        {
+                            stripe_customer_id:(await stripeCustomer).id,
+                        }
+                    }
+                )
+            )
+            customerId = (await stripeCustomer).id
+        }
 
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: (await stripeCustomer).id,
-            payment_method_types: ['card'], 
-            line_items:[
+            customer: customerId,
+            payment_method_types: ['card'],
+            line_items: [
                 { price: 'price_1LDXpRDjYhLl1Pr8auyEWDKk', quantity: 1 }
             ],
             mode: 'subscription',
@@ -26,7 +63,7 @@ export default async (req: NextApiRequest, res:NextApiResponse) => {
         })
 
         return res.status(200).json({ sessionId: stripeCheckoutSession.id })
-    }else {
+    } else {
         res.setHeader('Allow', 'POST')
         res.status(405).end('Method not allowed')
     }
